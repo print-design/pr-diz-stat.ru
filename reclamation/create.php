7f8055d6c698cf5720f8c18d92819b5fb135698e
Дмитрий <@DIMA>
2025-09-19 16:50:14 +0300

    2025-09-18 Рекламации

---
+++ reclamation/create.php
@@ -0,0 +1,26 @@
+<?php
+include '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE_NAMES[ROLE_MANAGER_SENIOR]))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+?>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+    </head>
+    <body>
+        <?php
+        include '../include/header.php';
+        ?>
+        <div class="container-fluid">
+            <h1>Новая рекламация</h1>
+        </div>
+        <?php
+        include '../include/footer.php';
+        ?>
+    </body>
+</html>
--- reclamation/index.php
+++ reclamation/index.php
@@ -16,6 +16,8 @@ if(!IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE
     <body>
         <?php
         include '../include/header.php';
+        include '../include/pager_top.php';
+        $rowcounter = 0;
         ?>
         <div class="container-fluid">
             <?php
@@ -23,7 +25,39 @@ if(!IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE
                 echo "<div class='alert alert-danger'>$error_message</div>";
             }
             ?>
-            <h1>Рекламации</h1>
+            <div class="d-flex justify-content-between mb-auto">
+                <div class="p-0">
+                    <h1>Рекламации</h1>
+                </div>
+                <div class="pt-1">
+                    <a href="create.php" class="btn btn-dark"><i class="fas fa-plus"></i>&nbsp;Новая рекламация</a>
+                </div>
+            </div>
+            <table class="table table-hover" id="content_table">
+                <thead>
+                    <tr style="border-top: 1px solid #dee2e6; border-left: 1px solid #dee2e6; border-right: 1px solid #dee2e6;">
+                        <th style="padding-left: 5px; padding-right: 5px;">Дата</th>
+                        <th style="padding-left: 5px; padding-right: 5px;">№ заказа</th>
+                        <th style="padding-left: 5px; padding-right: 5px;">Заказ</th>
+                        <th style="padding-left: 5px; padding-right: 5px;">Количество</th>
+                        <th style="padding-left: 5px; padding-right: 5px;">В процентах</th>
+                        <th style="padding-left: 5px; padding-right: 5px;">Этапы</th>
+                        <th style="padding-left: 5px; padding-right: 5px;">Комментарий</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $sql = "";
+                    ?>
+                </tbody>
+            </table>
+            <?php
+            include '../include/pager_bottom.php';
+            ?>
         </div>
+        <?php
+        include '../include/footer.php';
+        ?>
     </body>
 </html>
