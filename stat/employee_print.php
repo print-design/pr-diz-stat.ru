90ddb891d8cd121b626748b538b364717a0f7fad
Дмитрий <@DMITRY>
2025-11-24 14:52:43 +0300

    28.10.2025 stat

--- stat/employee_print.php
+++ stat/employee_print.php
@@ -47,8 +47,8 @@ if(!IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE
                                     . "inner join plan_edition ped on ped.calculation_id = c.id, "
                                     . "plan_workshift1 pw1 "
                                     . "inner join plan_employee pem on pw1.employee1_id = pem.id "
-                                    . "where pw1.date = ped.date and pw1.shift = ped.shift and pw1.work_id = ped.work_id and pw1.machine_id = ped.machine_id and ped.work_id = 1 "
-                                    . "group by pem.id "
+                                    . "where pw1.date = ped.date and pw1.shift = ped.shift and pw1.work_id = ped.work_id and pw1.machine_id = ped.machine_id and ped.work_id = ". WORK_PRINTING
+                                    . " group by pem.id "
                                     . "order by total desc";
                             $fetcher = new Fetcher($sql);
                             
@@ -84,8 +84,28 @@ if(!IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE
                                 </thead>
                                 <tbody>
                                     <?php
-                                    $sql = "";
+                                    $sql = "select pem.last_name, pem.first_name, count(r.id) total, "
+                                            . "count(r.id) / (select count(id) from reclamation) * 100 percent "
+                                            . "from calculation c "
+                                            . "inner join reclamation r on r.calculation_id = c.id "
+                                            . "inner join reclamation_defect rd on rd.reclamation_id = r.id "
+                                            . "inner join plan_edition ped on ped.calculation_id = c.id, "
+                                            . "plan_workshift1 pw1 "
+                                            . "inner join plan_employee pem on pw1.employee1_id = pem.id "
+                                            . "where rd.defect_type = ".$defect['defect_type']." and rd.other_defect_type = '".$defect['other_defect_type']."' and "
+                                            . "pw1.date = ped.date and pw1.shift = ped.shift and pw1.work_id = ped.work_id and pw1.machine_id = ped.machine_id and ped.work_id = ". WORK_PRINTING
+                                            . " group by pem.id "
+                                            . "order by total desc";
+                                    $fetcher = new Fetcher($sql);
+                                    
+                                    while($row = $fetcher->Fetch()):
                                     ?>
+                                    <tr>
+                                        <td><?=$row['last_name'].' '.$row['first_name'] ?></td>
+                                        <td><?=$row['total'] ?></td>
+                                        <td class="text-right"><?= DisplayNumber(floatval($row['percent']), 2) ?>%</td>
+                                    </tr>
+                                    <?php endwhile; ?>
                                 </tbody>
                             </table>
                         </div>
